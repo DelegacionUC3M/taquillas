65f7ea293c5fa63a5dc24a12cde50b7331513fee
Patataman <shas_arroyo@hotmail.com>
2015-06-30 18:29:06 +0200

    arreglos

--- app/controllers/adminController.php
+++ app/controllers/adminController.php
@@ -14,6 +14,7 @@
 				if (isset($_POST['busqueda'])) {
 					$error = '';
 					$search;
+					$listado;
 					if (empty($_POST['campus']) || empty($_POST['edificio']) || empty($_POST['planta']) 
 						|| empty($_POST['zona']) || empty($_POST['tipo']) || empty($_POST['user_id']) 
 						|| empty($_POST['num_taquilla']) || empty($_POST['fecha']) || empty($_POST['estado']) ) {
@@ -29,9 +30,10 @@
 						$search['num_taquilla'] = $_POST['num_taquilla'];
 						$search['fecha'] = "'".$_POST['fecha']."'";
 						$search['estado'] = $_POST['estado'];
+						//Taquillas resultantes de la busqueda
+						$listado = Taquilla::findByAttributes($search);
 					}
-					//Taquillas resultantes de la busqueda
-					$listado = Taquilla::findByAttributes($search);
+					
 					$this->render('listado',array('lista'=>$listado, 'error'=>$error));
 				}
 				else{
