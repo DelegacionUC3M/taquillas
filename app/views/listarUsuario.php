f6e5172091c237125bc27a3ac74000d87506c042
Patataman <shas_arroyo@hotmail.com>
2015-09-22 16:06:34 +0200

    css

--- app/views/listarUsuario.php
+++ app/views/listarUsuario.php
@@ -1,7 +1,7 @@
 	<h2> Usuarios </h2>
 	<ul class='menuHorizontal'>
 		<li class='nombre'> <b>Nombre</b> </li>
-		<li class='numero'> <b>NIA</b> </li>
+		<li class='nia'> <b>NIA</b> </li>
 		<li class='numero'> <b>Rol</b> </li>
 	</ul>
 
@@ -10,7 +10,7 @@
 		?>
 		<ul class='menuHorizontal2'>
 			<li class='nombre'> <?php echo $usuario['nombre'].' '.$usuario['apellido1'].' '.$usuario['apellido2']  ?></li> 
-			<li class='numero'> <?php echo $usuario['nia'] ?> </li>
+			<li class='nia'> <?php echo $usuario['nia'] ?> </li>
 			<li class='numero'> <?php echo $usuario['rol']  ?></li>
 			<li class='numero'> <a href='/taquillas/manager/modificarUsuario/<?php echo $usuario["id"]?>'> Modificar </a></li> 
 		</ul>
