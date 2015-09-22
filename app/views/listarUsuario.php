3a0be9fe641408a3615f030389e098f3f5290ad6
Patataman <shas_arroyo@hotmail.com>
2015-09-22 16:05:27 +0200

    css

--- app/views/listarUsuario.php
+++ app/views/listarUsuario.php
@@ -1,6 +1,6 @@
 	<h2> Usuarios </h2>
 	<ul class='menuHorizontal'>
-		<li class='numero'> <b>Nombre</b> </li>
+		<li class='nombre'> <b>Nombre</b> </li>
 		<li class='numero'> <b>NIA</b> </li>
 		<li class='numero'> <b>Rol</b> </li>
 	</ul>
@@ -9,7 +9,7 @@
 			foreach ($lista as $usuario){
 		?>
 		<ul class='menuHorizontal2'>
-			<li class='numero'> <?php echo $usuario['nombre'].' '.$usuario['apellido1'].' '.$usuario['apellido2']  ?></li> 
+			<li class='nombre'> <?php echo $usuario['nombre'].' '.$usuario['apellido1'].' '.$usuario['apellido2']  ?></li> 
 			<li class='numero'> <?php echo $usuario['nia'] ?> </li>
 			<li class='numero'> <?php echo $usuario['rol']  ?></li>
 			<li class='numero'> <a href='/taquillas/manager/modificarUsuario/<?php echo $usuario["id"]?>'> Modificar </a></li> 
