2cb47bdeefa6ab705ec53c1498c55fc9bce303f7
Patataman <shas_arroyo@hotmail.com>
2015-09-22 16:07:40 +0200

    css

--- app/views/listarUsuario.php
+++ app/views/listarUsuario.php
@@ -9,7 +9,7 @@
 			foreach ($lista as $usuario){
 		?>
 		<ul class='menuHorizontal2'>
-			<li class='nombre'> <?php echo $usuario['nombre'].' '.$usuario['apellido1'].' '.$usuario['apellido2']  ?></li> 
+			<li class='nombre'> <?php echo ucwords(strtolower($usuario['nombre'].' '.$usuario['apellido1'].' '.$usuario['apellido2']))  ?></li> 
 			<li class='nia'> <?php echo $usuario['nia'] ?> </li>
 			<li class='numero'> <?php echo $usuario['rol']  ?></li>
 			<li class='numero'> <a href='/taquillas/manager/modificarUsuario/<?php echo $usuario["id"]?>'> Modificar </a></li> 
