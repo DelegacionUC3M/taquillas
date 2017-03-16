8192fdf776bb23272b48ff1bf90b409632435797
Davinsky <davidb1mail@gmail.com>
2017-03-16 22:08:23 +0100

    Locker_crete - Basic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from models.connection import db
 
+from models.locker import Locker
 from models.place import Place
+from models.type import Type
 
 
 # Inicializacion del objeto Flask
@@ -22,9 +24,47 @@ def index():
 @app.route('/manager/taquillas/crear', methods=['GET', 'POST'])
 def locker_create():
     if request.method == 'POST':
-        print(request.form['school'])
-    p = Place.query.all()
-    return render_template('manager/locker_create.html', places=p)
+        place = Place.query.filter_by(school=request.form['school'])\
+                                .filter_by(building=request.form['building'])\
+                                .filter_by(floor=request.form['floor'])\
+                                .filter_by(zone=request.form['zone']).all()
+        if len(place) != 0:
+                locker = Locker(request.form['number'], 1, None, request.form['type'], place[0].id, None, None)
+                db.session.add(locker)
+                db.session.commit()
+        else:
+            print('Localización no válida')
+
+
+    schools = Place.query.with_entities(Place.school).all()
+    list_school = []
+    for school in schools:
+        if school.school not in list_school:
+            list_school.append(school.school)
+
+    buildings = Place.query.with_entities(Place.building).all()
+    list_building = []
+    for building in buildings:
+        if building.building not in list_building:
+            list_building.append(building.building)
+
+    floors = Place.query.with_entities(Place.floor).all()
+    list_floor = []
+    for floor in floors:
+        if floor.floor not in list_floor:
+            list_floor.append(floor.floor)
+
+    zones = Place.query.with_entities(Place.zone).all()
+    list_zone = []
+    for zone in zones:
+        if zone.zone not in list_zone:
+            list_zone.append(zone.zone)
+
+    types = Type.query.all()
+
+    return render_template('manager/locker_create.html',list_school=list_school,
+                           list_building=list_building, list_floor=list_floor,
+                           list_zone=list_zone, types=types)
 
 
 if __name__ == '__main__':
