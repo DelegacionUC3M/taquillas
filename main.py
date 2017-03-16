aaee432f8a8035fd4024eb8ad660ee4e1f78367a
Davinsky <davidb1mail@gmail.com>
2017-03-16 15:39:35 +0100

    Primeros pasos controladores

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from models.connection import db
+
+from models.place import Place
+
 
 # Inicializacion del objeto Flask
 app = Flask(__name__)
@@ -15,6 +18,14 @@ db.init_app(app)
 @app.route('/')
 def index():
     return 'Hola mundo'
+
+@app.route('/manager/taquillas/crear', methods=['GET', 'POST'])
+def locker_create():
+    if request.method == 'POST':
+        print(request.form['school'])
+    p = Place.query.all()
+    return render_template('manager/locker_create.html', places=p)
+
 
 if __name__ == '__main__':
     app.run()
